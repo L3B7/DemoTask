38d48d71af6e402dd71cd16a6f29da57abd52144
levib <levente.bolyki@gmail.com>
2025-03-03 15:44:10 +0100

    Android App functionality

--- android/SeonTask/app/src/main/java/com/example/seontask/MainActivity.java
+++ android/SeonTask/app/src/main/java/com/example/seontask/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.seontask;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Pair;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +20,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nativeLib = new NativeLib();
-        /* Log test
-        String input = "key1=value1;key2=value2;key3=value3";
+        // Log test
+/*        String input = "key1=value1;key2=value2;key3=value3";
         ArrayList<Pair<String, String>> parsedData = nativeLib.parseString(input);
-        Log.d("MainActivity", "Parsed data: " + parsedData);
-         */
+        Log.d("MainActivity", "Parsed data: " + parsedData);*/
+
+
+        EditText inputEditText = findViewById(R.id.inputEditText);
+        TextView outputTextView = findViewById(R.id.outputTextView);
+        Button convertButton = findViewById(R.id.convertButton);
+        convertButton.setOnClickListener(v -> {
+            inputEditText.setEnabled(false);
+            convertButton.setEnabled(false);
+            String input = inputEditText.getText().toString();
+            new Thread(() -> {
+                ArrayList<Pair<String, String>> parsedData = new ArrayList<>();
+                StringBuilder output = new StringBuilder();
+                try{
+                    parsedData = nativeLib.parseString(input);
+                } catch (Exception e) {
+                    Log.e("MainActivity", "Error parsing input", e);
+                    output.append("Error parsing input: ").append(e.getMessage()).append("\n");
+                }
+                if(parsedData.isEmpty()) {
+                    output.append("No data parsed\n");
+                }else {
+                    for (Pair<String, String> pair : parsedData) {
+                        output.append(pair.first).append(" = ").append(pair.second).append("\n");
+                    }
+                }
+
+                runOnUiThread(() -> {
+                    outputTextView.setText(output.toString());
+                    inputEditText.setEnabled(true);
+                    convertButton.setEnabled(true);
+                });
+            }).start();
+        });
+
+
+
 
     }
 }
